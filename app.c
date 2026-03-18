781df2dfaaeee21ffd017bd1ebac7150cafbdc3c
f8tum <parthnaik.office@gmail.com>
2026-03-18 09:16:15 +0530

    mvp struct definition

---
+++ app.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Struct definition
+struct Task
+{
+  int id;
+  char name[100];
+  char category[20];
+  int priority;
+  bool completed;
+};
+
+int main()
+{
+  
+  return 0;
+}
